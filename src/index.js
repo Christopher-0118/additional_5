8d394d32e5335a733c13833cfe5b0dcbb26c6130
Christopher-0118 <k.karantsevich@gmail.com>
2018-03-01 13:07:56 +0300

    correct-expression/feature: implement check method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let lengthStr = str.length;
+  let lengthBrConf = bracketsConfig.length;
+  let stack = [];
+  
+  for (let i = 0; i < lengthStr; i++) {
+    for (let j = 0; j < lengthBrConf; j++) {
+      let openingBracket = bracketsConfig[j][0];
+      let closingBracket = bracketsConfig[j][1];
+      let lengthStack = stack.length;
+
+      if (str[i] === openingBracket && i !== lengthStr - 1) {
+        stack.push(str[i]);
+      } else if (str[i] === closingBracket && stack[lengthStack - 1] === openingBracket) { // 
+        stack.pop();
+      } else if (str[i] === closingBracket && (lengthStack === 0 || stack[lengthStack - 1] !== openingBracket)){
+        return false;
+      }
+    }
+  }
+  if (stack.length > 0) {return false}
+  return true;
 }
