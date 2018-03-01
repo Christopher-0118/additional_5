f35e8739ede87ab1555aaae6ca0bfcef0ff019fe
Christopher-0118 <k.karantsevich@gmail.com>
2018-03-01 17:27:13 +0300

    correct-expression/fix: error

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ module.exports = function check(str, bracketsConfig) {
   let lengthStr = str.length;
   let lengthBrConf = bracketsConfig.length;
   let stack = [];
+  stack.push(str[0]);
   
-  for (let i = 0; i < lengthStr; i++) {
+  for (let i = 1; i < lengthStr; i++) {
     for (let j = 0; j < lengthBrConf; j++) {
       let openingBracket = bracketsConfig[j][0];
       let closingBracket = bracketsConfig[j][1];
       let lengthStack = stack.length;
+      let lastElement = stack[lengthStack - 1];
 
-      if (str[i] === openingBracket && i !== lengthStr - 1) {
-        stack.push(str[i]);
-      } else if (str[i] === closingBracket && stack[lengthStack - 1] === openingBracket) { // 
+      if (str[i] === closingBracket && lastElement === openingBracket) {
         stack.pop();
-      } else if (str[i] === closingBracket && (lengthStack === 0 || stack[lengthStack - 1] !== openingBracket)){
+      } else if (str[i] === openingBracket || (str[i] === openingBracket && i < lengthStr - 1)) {
+        stack.push(str[i]);
+      } else if ((str[i] === closingBracket && lastElement !== openingBracket) || (str[i] === openingBracket && i === lengthStr - 1)) {
         return false;
       }
     }
   }
-  if (stack.length > 0) {return false}
   return true;
 }
