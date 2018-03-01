e1359c0d1335a86cb118698cde8a36e530d370f2
Krystsina Karantsevich <k.karantsevich@gmail.com>
2018-03-01 20:07:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ module.exports = function check(str, bracketsConfig) {
       let lengthStack = stack.length;
       let lastElement = stack[lengthStack - 1];
 
-      //___it's a closing bracket  and  the last element in this stack is right opening bracket
+      //___it's a closing bracket  and  the last element in this stack is correct opening bracket
       if (str[i] === closingBracket && lastElement === openingBracket) { // 
         stack.pop();
       // ________it's an opening bracket and it isn't the last in this string
-      } else if (str[i] === openingBracket && (str[i] === openingBracket && i < lengthStr - 1)) { 
+      } else if (str[i] === openingBracket && i < lengthStr - 1) { 
         stack.push(str[i]);  
-      // _________it's closing bracket, but it isn't right bracket________________it's an opening bracket, but it is the last in this string
+      // _________it's closing bracket, but it isn't correct bracket______________it's an opening bracket, but it is the last in this string
       } else if ((str[i] === closingBracket && lastElement !== openingBracket) || (str[i] === openingBracket && i === lengthStr - 1)) {
         return false;
       } 
