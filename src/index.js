798287e2c73e3d8a7b8343f9cf73821b34217833
Christopher-0118 <k.karantsevich@gmail.com>
2018-03-01 18:01:10 +0300

    correct-expression/fix: all error

--- src/index.js
+++ src/index.js
@@ -11,13 +11,16 @@ module.exports = function check(str, bracketsConfig) {
       let lengthStack = stack.length;
       let lastElement = stack[lengthStack - 1];
 
-      if (str[i] === closingBracket && lastElement === openingBracket) {
+      //___it's a closing bracket  and  the last element in this stack is right opening bracket
+      if (str[i] === closingBracket && lastElement === openingBracket) { // 
         stack.pop();
-      } else if (str[i] === openingBracket || (str[i] === openingBracket && i < lengthStr - 1)) {
-        stack.push(str[i]);
+      // ________it's an opening bracket and it isn't the last in this string
+      } else if (str[i] === openingBracket && (str[i] === openingBracket && i < lengthStr - 1)) { 
+        stack.push(str[i]);  
+      // _________it's closing bracket, but it isn't right bracket________________it's an opening bracket, but it is the last in this string
       } else if ((str[i] === closingBracket && lastElement !== openingBracket) || (str[i] === openingBracket && i === lengthStr - 1)) {
         return false;
-      }
+      } 
     }
   }
   return true;
